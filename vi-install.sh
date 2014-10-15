10a80f261d2938478618ed313e2e61eb1ff91b0f
Tom Deckers <tom@ducbase.com>
2014-10-15 13:26:14 +0200

    Follow redirects and remove Vundle before reinstalling

--- vi-install.sh
+++ vi-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+rm -rf ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-curl -o ~/.vim/vimrc https://github.com/tdeckers/guides/raw/master/vimrc
+curl -L -o ~/.vim/vimrc https://github.com/tdeckers/guides/raw/master/vimrc
