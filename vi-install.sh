a96e427972a9764c2ea2cde20f9a7af6cfd4d1f5
Tom Deckers <tom@ducbase.com>
2014-10-15 13:22:56 +0200

    use raw link

--- vi-install.sh
+++ vi-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-curl -o ~/.vim/vimrc https://github.com/tdeckers/guides/blob/master/vimrc
+curl -o ~/.vim/vimrc https://github.com/tdeckers/guides/raw/master/vimrc
