b6aef34947474c9f0727824aa639ad82b41b94f0
Tom Deckers <tom@ducbase.com>
2014-10-15 13:30:50 +0200

    Run PluginInstall after installation

--- vi-install.sh
+++ vi-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-rm -rf ~/.vim/bundle
+rm -rf ~/.vim/bundle/
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -L -o ~/.vim/vimrc https://github.com/tdeckers/guides/raw/master/vimrc
+
+vim +BundleInstall +qall
