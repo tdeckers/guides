8df027e29476973c4ce8bbf2a6f2aa40374c52c1
Tom Deckers <tom@ducbase.com>
2014-11-04 10:50:28 +0100

    Update vi-install.sh

--- vi-install.sh
+++ vi-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf ~/.vim/bundle/
+mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -L -o ~/.vim/vimrc https://github.com/tdeckers/guides/raw/master/vimrc
 
